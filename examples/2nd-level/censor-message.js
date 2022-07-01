46e8c0b76b689c422fa4afb02de3d3285d6c623b
Niamh <CORP\2206-022-nb@a-134dk3qbze3b6.corp.amazonworkspaces.com>
2022-07-01 17:09:27 +0100

    Add finalised censor-message

--- examples/2nd-level/censor-message.js
+++ examples/2nd-level/censor-message.js
@@ -32,11 +32,27 @@ for each string, WORD, in RAW_WORDS
 set SCRUBBED_MESSAGE to the string created by joining together every string in SCRUBBED_WORDS with a space (' ') as a join character
 
 return SCRUBBED_MESSAGE
-
-test cases:
-===========
-censorMessage("don't mess with cats", ["mess"]) should be "don't ***** with cats"
-censorMessage("Are you sure that this is safe", ["are", "is"]) should be "***** you sure that this ***** safe"
-censorMessage("I CANNOT HELP YOU", ["can", "not", "help", "you"]) should be "I CANNOT ***** *****"
-
 */
+
+function censorMessage(message, banList) {
+    let rawWords = message.split(" ")
+    let scrubbedWords = []
+
+    for (let word of rawWords) {
+        let lowercaseWord = word.toLowerCase()
+        
+        if (banList.includes(lowercaseWord)) {
+            scrubbedWords.push('******')
+        } else {
+            scrubbedWords.push(word)
+        }
+    }
+
+    let scrubbedMessage = scrubbedWords.join(" ")
+    return scrubbedMessage
+}
+
+// test cases:
+console.log(censorMessage("don't mess with cats", ["mess"]), 'should be "dont ***** with cats"')
+console.log(censorMessage("Are you sure that this is safe", ["are", "is"]), 'should be "***** you sure that this ***** safe"')
+console.log(censorMessage("I CANNOT HELP YOU", ["can", "not", "help", "you"]), 'should be "I CANNOT ***** *****"')

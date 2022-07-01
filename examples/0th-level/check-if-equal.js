2c2b8c53c80ff9b49f819027f6322a6219c9fb73
Niamh <CORP\2206-022-nb@a-134dk3qbze3b6.corp.amazonworkspaces.com>
2022-07-01 16:28:19 +0100

    Add complete check-if-equal

--- examples/0th-level/check-if-equal.js
+++ examples/0th-level/check-if-equal.js
@@ -19,12 +19,21 @@ if NUM_ONE is equal to NUM_TWO
 	return True
 else
 	return False
-
-test cases:
-===========
-isEqual(4, 4) should be True
-isEqual(0, 0) should be True
-isEqual(-4, -4) should be True
-isEqual(4, -4) should be False
-isEqual(0, 1) should be False
 */
+
+function isEqual(num1, num2) {
+    if (num1 === num2) {
+        return true
+    }
+
+    return false
+}
+
+
+// test cases:
+
+console.log(isEqual(4, 4), 'should be True')
+console.log(isEqual(0, 0), 'should be True')
+console.log(isEqual(-4, -4), 'should be True')
+console.log(isEqual(4, -4), 'should be False')
+console.log(isEqual(0, 1), 'should be False')
